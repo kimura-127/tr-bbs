99a5bd0fa77535027ee29c413b624e1238c17d8d
木村愛哉 <kimuramanaya@kimulocal.local>
2025-01-08 01:20:36 +0900

    feat: トップページ作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,68 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { columns, type Payment } from "@/components/columns"
+import { DataTable } from "@/components/data-table"
+import { Button } from "@/components/ui/button"
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Pen, Search, SquarePen } from "lucide-react"
+
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      title:"とぴっく",name:"名無し",replyCount:3,createdAt:new Date()
+    },
+    // ...
+  ]
+}
 
 export default async function Home() {
+  const data = await getData()
+
   return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
-  );
+    <div className="container mx-auto py-10">
+      <div className="flex bg-gray-700 rounded-lg px-2 py-1.5 mb-6">
+        <div>
+          <Dialog>
+            <DialogTrigger asChild>
+             <Button className="h-8 bg-gray-700 hover:bg-gray-800 font-semibold gap-2 text-base tracking-wide">
+               <SquarePen />
+               新規作成
+             </Button>
+            </DialogTrigger>
+            <DialogContent  className="max-w-4xl max-h-screen overflow-auto">
+              <DialogHeader>
+                <DialogTitle>新規スレッド作成</DialogTitle>
+                <DialogDescription className="py-2">
+                  タイトルとコメントを入力してください
+                </DialogDescription>
+              </DialogHeader>
+              <div className="my-2 font-semibold tracking-wide">
+                <p>タイトル</p>
+                <Input />
+              </div>
+              <div className="mb-4 font-semibold tracking-wider">
+                <p>コメント</p>
+                <Textarea className="h-40" />
+              </div>
+              <div className="flex justify-center">
+              <Button className="h-12 w-80 bg-gray-700 hover:bg-gray-800 font-semibold gap-2 text-base tracking-wide">
+                  スレッド作成
+                </Button>
+              </div>
+            </DialogContent>
+          </Dialog>
+        
+        </div>
+        <div>
+        <Button className="h-8 bg-gray-700 hover:bg-gray-800 font-semibold gap-2 text-base tracking-wide">
+          <Search />
+            検索
+          </Button>
+        </div>
+      </div>
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
 }
--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -1,5 +1,4 @@
 import { signOutAction } from "@/app/actions";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import Link from "next/link";
 import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
@@ -12,7 +11,7 @@ export default async function AuthButton() {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (!hasEnvVars) {
+  
     return (
       <>
         <div className="flex gap-4 items-center">
@@ -47,24 +46,4 @@ export default async function AuthButton() {
         </div>
       </>
     );
-  }
-  return user ? (
-    <div className="flex items-center gap-4">
-      Hey, {user.email}!
-      <form action={signOutAction}>
-        <Button type="submit" variant={"outline"}>
-          Sign out
-        </Button>
-      </form>
-    </div>
-  ) : (
-    <div className="flex gap-2">
-      <Button asChild size="sm" variant={"outline"}>
-        <Link href="/sign-in">Sign in</Link>
-      </Button>
-      <Button asChild size="sm" variant={"default"}>
-        <Link href="/sign-up">Sign up</Link>
-      </Button>
-    </div>
-  );
 }
