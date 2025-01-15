91baf0b4c114d55d80ca305d6cf40f676f767ac6
木村愛哉 <kimuramanaya@kimulocal.local>
2025-01-16 02:20:28 +0900

    feat: 記事作成が反映されるように実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,8 @@
-import { CreateThreadForm } from '@/components/CreateThreadForm';
-import { type Payment, columns } from '@/components/columns';
-import { DataTable } from '@/components/data-table';
-import { Button } from '@/components/ui/button';
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog';
-import { Search, SquarePen } from 'lucide-react';
-
-async function getData(): Promise<Payment[]> {
-  return [
-    {
-      title: 'とぴっく',
-      name: '名無し',
-      replyCount: 3,
-      createdAt: new Date(),
-    },
-  ];
-}
+import { ThreadList } from '@/components/ThreadList';
+import { getThreads } from './actions/getThread';
 
 export default async function Home() {
-  const data = await getData();
+  const data = await getThreads();
 
-  return (
-    <div className="container mx-auto py-10">
-      <div className="flex gap-1 bg-gray-700 rounded-lg px-2 py-1.5 mb-6">
-        <div>
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button className="h-8 bg-gray-700 hover:bg-gray-800 font-semibold gap-2 text-base tracking-wide">
-                <SquarePen />
-                新規作成
-              </Button>
-            </DialogTrigger>
-            <DialogContent className="max-w-4xl max-h-screen overflow-auto">
-              <DialogHeader>
-                <DialogTitle>新規スレッド作成</DialogTitle>
-                <DialogDescription className="py-2">
-                  タイトルとコメントを入力してください
-                </DialogDescription>
-              </DialogHeader>
-              <CreateThreadForm />
-            </DialogContent>
-          </Dialog>
-        </div>
-        <div>
-          <Button className="h-8 bg-gray-700 hover:bg-gray-800 font-semibold gap-2 text-base tracking-wide">
-            <Search />
-            検索
-          </Button>
-        </div>
-      </div>
-      <DataTable columns={columns} data={data} />
-    </div>
-  );
+  return <ThreadList initialData={data} />;
 }
---
+++ components/ThreadList.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { CreateThreadForm } from '@/components/CreateThreadForm';
+import { type Payment, columns } from '@/components/columns';
+import { DataTable } from '@/components/data-table';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Search, SquarePen } from 'lucide-react';
+import { useState } from 'react';
+
+type Props = {
+  initialData: Payment[];
+};
+
+export function ThreadList({ initialData }: Props) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  return (
+    <div className="container mx-auto py-10">
+      <div className="flex gap-1 bg-gray-700 rounded-lg px-2 py-1.5 mb-6">
+        <div>
+          <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+            <DialogTrigger asChild>
+              <Button className="h-8 bg-gray-700 hover:bg-gray-800 font-semibold gap-2 text-base tracking-wide">
+                <SquarePen />
+                新規作成
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="max-w-4xl max-h-screen overflow-auto">
+              <DialogHeader>
+                <DialogTitle>新規スレッド作成</DialogTitle>
+                <DialogDescription className="py-2">
+                  タイトルとコメントを入力してください
+                </DialogDescription>
+              </DialogHeader>
+              <CreateThreadForm setIsDialogOpen={setIsDialogOpen} />
+            </DialogContent>
+          </Dialog>
+        </div>
+        <div>
+          <Button className="h-8 bg-gray-700 hover:bg-gray-800 font-semibold gap-2 text-base tracking-wide">
+            <Search />
+            検索
+          </Button>
+        </div>
+      </div>
+      <DataTable columns={columns} data={initialData} />
+    </div>
+  );
+}
