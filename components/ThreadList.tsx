3158c852674ed954a2d275edb59910dbdcae5fbc
木村愛哉 <kimuramanaya@kimulocal.local>
2025-01-16 04:09:42 +0900

    feat: サイトロゴの追加

--- components/ThreadList.tsx
+++ components/ThreadList.tsx
@@ -13,7 +13,20 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 import { Search, SquarePen } from 'lucide-react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+
+function MyImage() {
+  return (
+    <Image
+      src="/me.png" // 画像のパス
+      alt="Picture of the author" // 代替テキスト（必須）
+      width={500} // 幅（必須）
+      height={500} // 高さ（必須）
+    />
+  );
+}
 
 type Props = {
   initialData: Payment[];
@@ -21,14 +34,28 @@ type Props = {
 
 export function ThreadList({ initialData }: Props) {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const router = useRouter();
+
+  const handleLogoClick = () => {
+    router.refresh();
+    router.push('/');
+  };
 
   return (
-    <div className="container mx-auto py-10">
+    <div className="container mx-auto py-2">
+      <Image
+        src="/images/site-logo-white.png"
+        alt="logo"
+        height={40}
+        width={100}
+        className="mb-1.5 cursor-pointer"
+        onClick={handleLogoClick}
+      />
       <div className="flex gap-1 bg-gray-700 rounded-lg px-2 py-1.5 mb-6">
         <div>
           <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
             <DialogTrigger asChild>
-              <Button className="h-8 bg-gray-700 hover:bg-gray-800 font-semibold gap-2 text-base tracking-wide">
+              <Button className="h-8 bg-gray-700 hover:bg-gray-700 hover:text-gray-300 font-semibold gap-2 text-base tracking-wide">
                 <SquarePen />
                 新規作成
               </Button>
@@ -45,7 +72,7 @@ export function ThreadList({ initialData }: Props) {
           </Dialog>
         </div>
         <div>
-          <Button className="h-8 bg-gray-700 hover:bg-gray-800 font-semibold gap-2 text-base tracking-wide">
+          <Button className="h-8 bg-gray-700 hover:bg-gray-700 hover:text-gray-300 font-semibold gap-2 text-base tracking-wide">
             <Search />
             検索
           </Button>
